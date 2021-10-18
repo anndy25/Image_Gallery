331d3ea332684bb3099c61049d2ad3dd7ce6262f
Aniket Mane <anuketsmd310@gmail.com>
2021-10-18 23:15:22 +0530

    Image Gallery Version 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 
 import Header from './components/Header'
 import Dashboard from './components/DashBoard';
